fa28bd30f7e9e7f72cdc0a226886463ec7b0b6c4
iThorgrim <contact@warlog.dev>
2026-07-07 14:14:22 +0200

    refactor(LuaEngine): Remove unused method includes from LuaFunctions

---
+++ src/LuaEngine/methods/ALERegistry.h
@@ -0,0 +1,49 @@
+/*
+* Copyright (C) 2010 - 2025 Eluna Lua Engine <https://elunaluaengine.github.io/>
+* This program is free software licensed under GPL version 3
+* Please see the included DOCS/LICENSE.md for more information
+*/
+
+#ifndef ALEREGISTRY_H
+#define ALEREGISTRY_H
+
+#include <sol/forward.hpp>
+
+/*
+ * One registration function per Lua type, each defined at the bottom of its
+ * methods/<Type>Methods.cpp file. RegisterFunctions (LuaFunctions.cpp) calls
+ * them all on engine startup.
+ */
+
+void RegisterAchievementMethods(sol::state& lua);
+void RegisterALEQueryMethods(sol::state& lua);
+void RegisterAuraMethods(sol::state& lua);
+void RegisterBattleGroundMethods(sol::state& lua);
+void RegisterChatHandlerMethods(sol::state& lua);
+void RegisterCorpseMethods(sol::state& lua);
+void RegisterCreatureMethods(sol::state& lua);
+void RegisterGameObjectMethods(sol::state& lua);
+void RegisterGemPropertiesEntryMethods(sol::state& lua);
+void RegisterGlobalMethods(sol::state& lua);
+void RegisterGroupMethods(sol::state& lua);
+void RegisterGuildMethods(sol::state& lua);
+void RegisterItemMethods(sol::state& lua);
+void RegisterItemTemplateMethods(sol::state& lua);
+void RegisterLootMethods(sol::state& lua);
+void RegisterMapMethods(sol::state& lua);
+void RegisterObjectMethods(sol::state& lua);
+void RegisterPetMethods(sol::state& lua);
+void RegisterPlayerMethods(sol::state& lua);
+void RegisterQuestMethods(sol::state& lua);
+void RegisterRollMethods(sol::state& lua);
+void RegisterSpellEntryMethods(sol::state& lua);
+void RegisterSpellInfoMethods(sol::state& lua);
+void RegisterSpellMethods(sol::state& lua);
+void RegisterTicketMethods(sol::state& lua);
+void RegisterTransportMethods(sol::state& lua);
+void RegisterUnitMethods(sol::state& lua);
+void RegisterVehicleMethods(sol::state& lua);
+void RegisterWorldObjectMethods(sol::state& lua);
+void RegisterWorldPacketMethods(sol::state& lua);
+
+#endif // ALEREGISTRY_H
